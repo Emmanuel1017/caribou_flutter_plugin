364fac8330e065d6a92edf5c6b8e5eb1c0e7df98
emmanuel1017 <koriremmanuel@rcketmail.com>
2026-04-17 15:17:50 +0300

    fixed clashing packages

--- android/src/main/kotlin/com/caribou/caribou_flutter_plugin/CaribouPlugin.kt
+++ android/src/main/kotlin/com/caribou/caribou_flutter_plugin/CaribouPlugin.kt
@@ -52,7 +52,7 @@ import java.nio.ByteBuffer
 
 
 /** caribouPlugin */
-open class caribouPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+open class CaribouPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
